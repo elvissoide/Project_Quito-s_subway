b90efc4e1219ec6b7b061c8b73e2159fd7057860
Roberto Shiao <95731527+Shiao-Li@users.noreply.github.com>
2023-08-13 09:44:47 -0500

    Add files via upload
    integración react hook form y sweetalert2

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,212 +1,187 @@
-import Mensajes from "./Mensajes"
-import { useState } from "react"
-import { v4 as uuidv4 } from 'uuid';
-import { useEffect } from 'react'
-import Swal from 'sweetalert2'
+import Mensajes from "./Mensajes";
+import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
+import { useEffect } from "react";
+import Swal from 'sweetalert2';
 
 export const Formulario = ({ setEstado, idMetro, setIdMetro }) => {
-    // UseState
-    const [error, setError] = useState(false) // Mensajes de error
-    const [mensaje, setMensaje] = useState(false) // Mensajes de éxito
-    const [form, setform] = useState({ // Formulario
-        nombre: "demo",
-        sector: "",
-        salida: "",
-        llegada: "",
-        maquinista: "",
-        detalles: ""
-    })
-    // UseEffect
-    useEffect(() => {
-        console.log(idMetro);
-        if (idMetro) {
-            (async function (idMetro) {
-                try {
-                    const respuesta = await (await fetch(`http://localhost:3000/metro/${idMetro}`)).json()
-                    const { id, nombre, sector, salida, llegada, maquinista, detalles } = respuesta
-                    setform({
-                        ...form,
-                        nombre,
-                        sector,
-                        salida,
-                        llegada,
-                        maquinista,
-                        detalles,
-                        id
-                    })
-                }
-                catch (error) {
-                    Swal.fire(
-                        'ERROR',
-                        'Ha ocurrido un error al intentar obtener los datos del registro.',
-                        'error'
-                    )
-                }
-            })(idMetro)
-        }
-    }, [idMetro])
-    // Handle
-    const handleChange = (e) => {
-        setform({
-            ...form,
-            [e.target.name]: e.target.value.trim()
-        })
-    }
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if (Object.values(form).includes("") || Object.entries(form).length === 0) {
-            setError(true)
-            setTimeout(() => {
-                setError(false)
-            }, 1000);
-            return
-        }
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+
+  useEffect(() => {
+    console.log(idMetro);
+    if (idMetro) {
+      (async function (idMetro) {
         try {
-            // Actualizacion de las rutas
-            if (form.id) {
-                const url = `http://localhost:3000/metro/${form.id}`
-                await fetch(url, {
-                    method: 'PUT',
-                    body: JSON.stringify(form),
-                    headers: { 'Content-Type': 'application/json' }
-                })
-                if (!respuesta.ok) {
-                    throw new Error('La solicitud no fue exitosa');
-                }
-                setEstado(true)
-                setform({})
-                setIdMetro(null);
-                setTimeout(() => {
-                    setEstado(false)
-                    setform({})
-                }, 1000)
-            }
-            // Creacion de las rutas
-            else {
-                const url = "http://localhost:3000/metro"
-                form.id = uuidv4()
-                const respuesta = await fetch(url, {
-                    method: 'POST',
-                    body: JSON.stringify(form),
-                    headers: { 'Content-Type': 'application/json' }
-                })
-                if (!respuesta.ok) {
-                    throw new Error('La solicitud no fue exitosa');
-                }
-                setMensaje(true)
-                setEstado(true)
-                setTimeout(() => {
-                    setMensaje(false)
-                    setEstado(false)
-                    setform({})
-                }, 1000);
-            }
+          const respuesta = await (await fetch(`http://localhost:3000/metro/${idMetro}`)).json();
+          const { id, nombre, sector, salida, llegada, maquinista, detalles } = respuesta;
+          setValue("nombre", nombre);
+          setValue("sector", sector);
+          setValue("salida", salida);
+          setValue("llegada", llegada);
+          setValue("maquinista", maquinista);
+          setValue("detalles", detalles);
+          setIdMetro(id);
         } catch (error) {
-            Swal.fire(
-                'ERROR',
-                'Ha ocurrido un error al intentar actualizar o crear el registro.',
-                'error'
-            )
+          Swal.fire(
+            'ERROR',
+            'Ha ocurrido un error al intentar obtener los datos del registro.',
+            'error'
+          )
         }
+      })(idMetro);
+    }
+  }, [idMetro, setIdMetro]);
 
+  const onSubmit = async (data) => {
+    if (Object.values(data).includes("")) {
+      return;
     }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            {error && <Mensajes tipo="bg-red-900">"Existen campos vacíos"</Mensajes>}
-            {mensaje && <Mensajes tipo="bg-green-900">"Registro exitoso"</Mensajes>}
-            <div>
-                <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
-                <input
-                    id='nombre'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre de la ruta'
-                    name='nombre'
-                    value={form.nombre || ""}
-                    onChange={handleChange}
-                />
-            </div>
+    try {
+      // Actualización de las rutas
+      if (idMetro) {
+        const url = `http://localhost:3000/metro/${idMetro}`;
+        await fetch(url, {
+          method: "PUT",
+          body: JSON.stringify(data),
+          headers: { "Content-Type": "application/json" },
+        });
+        setEstado(true);
+        setIdMetro(null);
+      }
+      // Creación de las rutas
+      else {
+        const url = "http://localhost:3000/metro";
+        data.id = uuidv4();
+        await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: { "Content-Type": "application/json" },
+        });
+        setEstado(true);
+      }
 
-            <div>
-                <label
-                    htmlFor='sector'
-                    className='text-gray-700 uppercase font-bold text-sm'>Sector: </label>
-                <input
-                    id='sector'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='sector de la ruta'
-                    name='sector'
-                    value={form.sector || ""}
-                    onChange={handleChange}
-                />
-            </div>
+      setTimeout(() => {
+        setEstado(false);
+        setValue("nombre", "");
+        setValue("sector", "");
+        setValue("salida", "");
+        setValue("llegada", "");
+        setValue("maquinista", "");
+        setValue("detalles", "");
+      }, 1000);
+    } catch (error) {
+      Swal.fire(
+        'ERROR',
+        'Ha ocurrido un error al intentar actualizar o crear el registro.',
+        'error'
+      )
+    }
+  };
 
-            <div>
-                <label
-                    htmlFor='salida'
-                    className='text-gray-700 uppercase font-bold text-sm'>Punto de salida: </label>
-                <input
-                    id='salida'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='punto de salida'
-                    name='salida'
-                    value={form.salida || ""}
-                    onChange={handleChange}
-                />
-            </div>
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        {/* Campo Nombre */}
+        <label htmlFor="nombre" className="text-gray-700 uppercase font-bold text-sm">
+          Nombre:
+        </label>
+        <input
+          id="nombre"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.nombre ? "border-red-500" : ""
+            }`}
+          placeholder="nombre de la ruta"
+          {...register("nombre", { required: true })}
+        />
+        {errors.nombre && <span className="text-red-500">Campo requerido</span>}
+      </div>
 
-            <div>
-                <label
-                    htmlFor='llegada'
-                    className='text-gray-700 uppercase font-bold text-sm'>Punto de llegada: </label>
-                <input
-                    id='llegada'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='punto de llegada'
-                    name='llegada'
-                    value={form.llegada || ""}
-                    onChange={handleChange}
-                />
-            </div>
+      <div>
+        {/* Campo Sector */}
+        <label htmlFor="sector" className="text-gray-700 uppercase font-bold text-sm">
+          Sector:
+        </label>
+        <input
+          id="sector"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.sector ? "border-red-500" : ""
+            }`}
+          placeholder="sector de la ruta"
+          {...register("sector", { required: true })}
+        />
+        {errors.sector && <span className="text-red-500">Campo requerido</span>}
+      </div>
 
-            <div>
-                <label
-                    htmlFor='maquinista'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre del maquinista: </label>
-                <input
-                    id='maquinista'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre del maquinista'
-                    name='maquinista'
-                    value={form.maquinista || ""}
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label
-                    htmlFor='detalles'
-                    className='text-gray-700 uppercase font-bold text-sm'>Detalles: </label>
-                <textarea
-                    id='detalles'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    name='detalles'
-                    value={form.detalles || ""}
-                    onChange={handleChange}
-                />
-            </div>
+      <div>
+        {/* Campo Punto de Salida */}
+        <label htmlFor="salida" className="text-gray-700 uppercase font-bold text-sm">
+          Punto de salida:
+        </label>
+        <input
+          id="salida"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.salida ? "border-red-500" : ""
+            }`}
+          placeholder="punto de salida"
+          {...register("salida", { required: true })}
+        />
+        {errors.salida && <span className="text-red-500">Campo requerido</span>}
+      </div>
 
-            <input type="submit"
-                className='bg-sky-900 w-full p-3 text-white uppercase font-bold rounded-lg hover:bg-red-900 cursor-pointer transition-all'
-                value={form.id ? "Actualizar ruta" : "Registrar ruta"} />
+      <div>
+        {/* Campo Punto de Llegada */}
+        <label htmlFor="llegada" className="text-gray-700 uppercase font-bold text-sm">
+          Punto de llegada:
+        </label>
+        <input
+          id="llegada"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.llegada ? "border-red-500" : ""
+            }`}
+          placeholder="punto de llegada"
+          {...register("llegada", { required: true })}
+        />
+        {errors.llegada && <span className="text-red-500">Campo requerido</span>}
+      </div>
 
-        </form>
-    )
-}
+      <div>
+        {/* Campo Nombre del Maquinista */}
+        <label htmlFor="maquinista" className="text-gray-700 uppercase font-bold text-sm">
+          Nombre del maquinista:
+        </label>
+        <input
+          id="maquinista"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.maquinista ? "border-red-500" : ""
+            }`}
+          placeholder="nombre del maquinista"
+          {...register("maquinista", { required: true })}
+        />
+        {errors.maquinista && <span className="text-red-500">Campo requerido</span>}
+      </div>
+
+      <div>
+        {/* Campo Detalles */}
+        <label htmlFor="detalles" className="text-gray-700 uppercase font-bold text-sm">
+          Detalles:
+        </label>
+        <textarea
+          id="detalles"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.detalles ? "border-red-500" : ""
+            }`}
+          placeholder="detalles"
+          {...register("detalles", { required: true })}
+        />
+        {errors.detalles && <span className="text-red-500">Campo requerido</span>}
+      </div>
+
+      {/* Botón de Envío */}
+      <input
+        type="submit"
+        className="bg-sky-900 w-full p-3 text-white uppercase font-bold rounded-lg hover:bg-red-900 cursor-pointer transition-all"
+        value={idMetro ? "Actualizar ruta" : "Registrar ruta"}
+      />
+    </form>
+  );
+};
